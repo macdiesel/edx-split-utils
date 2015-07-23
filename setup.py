3e5c95fcf0faf2b29918eeda15e9bae4c21d8f69
Brian Beggs <bbeggs@edx.org>
2015-07-23 12:35:35 -0400

    initial split-utils structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Setup script for split-utils 
+"""
+
+from setuptools import setup
+
+setup(
+    name="split-utils",
+    version="0.1",
+    description="Utilities and visualizations for split mongo courses"
+    install_requires=["distribute"],
+    requires=[],
+)
+
