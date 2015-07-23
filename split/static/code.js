dfb2b86a1300a84c44c247dc228be4fe933a4f30
Diana Huang <dkh@edx.org>
2015-07-23 16:12:16 -0400

    Add node selection logic to the page.

--- split/static/code.js
+++ split/static/code.js
@@ -1,4 +1,7 @@
 $(function() {
+    // Hide the info pane initially eles.one()
+    $("#info-pane").hide();
+    // Create and set up the graph.
     $('#cy').cytoscape({
         style: cytoscape.stylesheet()
             .selector('node')
@@ -19,6 +22,12 @@ $(function() {
         ready: function() {
             window.cy = this;
             cy.elements().unselectify();
+            cy.on('tap', function(e){
+                var node = e.cyTarget;
+                $("#info-pane").text(node.data('name'));
+                $("#info-pane").show();
+
+            });
             cy.add([
                     { group: "nodes", data: { id: "n1", name: "node 1" }, position: { x: 100, y: 100 } },
                     { group: "nodes", data: { id: "n2", name: "node 2" }, position: { x: 100, y: 200 } },
