0e85e6e82842969d20ae3db0edbf4b991accedcb
Brian Beggs <bbeggs@edx.org>
2015-07-24 12:46:09 -0400

    first stab at trying to get the graph to load with xblock data

--- static/code.js
+++ static/code.js
@@ -1,37 +1,53 @@
-$(function() {
+$(function () {
     // Hide the info pane initially eles.one()
     $("#info-pane").hide();
+
+    //Get the info to load the graph
+    nodes = [];
+    edges = [];
+    elements = null;
+    $.ajax({url: "http://localhost:5000/api/v1/history/all/course-v1:edX+test105+2015_Q2"})
+        .done(function (data) {
+            $.each(data, function(key, value) {
+                if(key.toLowerCase() == 'root') {
+                     return true;
+                }
+
+                nodes.push({data: {id: key, name: key}});
+                console.log("key: " + key + " -- values: " + value)
+                if(value.length > 0) {
+                    $.each(value, function(index, val) {
+                        edges.push({data: {id: "e" + val, source: key, target: val}});
+                    });
+                }
+            });
+            elements = {nodes: nodes, edges: edges};
+            console.log(elements);
+        });
+
     // Create and set up the graph.
     $('#cy').cytoscape({
         style: cytoscape.stylesheet()
             .selector('node')
-                .css({
-                    'content': 'data(name)',
-                    'text-valign': 'center'
-                })
+            .css({
+                'content': 'data(name)',
+                'text-valign': 'center'
+            })
             .selector('edge')
-                .css({
-                    'target-arrow-shape': 'triangle'
-                }),
+            .css({
+                'target-arrow-shape': 'triangle'
+            }),
 
-        elements: {
-            nodes: [
-                {data: { id: "n1", name: "node 1" }},
-                {data: { id: "n2", name: "node 2" }}
-            ],
-            edges: [
-                {data: { id: "e1", source: "n1", target: "n2" }}
-            ]
-        },
+        elements: elements,
 
         layout: {
             name: 'dagre',
             rankDir: 'TB'
         },
-        ready: function() {
+        ready: function () {
             window.cy = this;
             cy.elements().unselectify();
-            cy.on('tap', function(e){
+            cy.on('tap', function (e) {
                 var node = e.cyTarget;
                 $("#info-pane").text(node.data('name'));
                 $("#info-pane").show();
